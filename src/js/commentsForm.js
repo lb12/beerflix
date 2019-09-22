373fb9a188594a89d5b8daaf48e9972b919a0276
David Escribano Rodriguez <david.escribanor@gmail.com>
2019-09-22 19:27:23 +0200

    Added validation into comment beers

--- src/js/commentsForm.js
+++ src/js/commentsForm.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import { setBeerComment } from './api.js';
-import { renderLoader } from './ui.js';
+import { toggle, renderLoader } from './ui.js';
 
 /**
  * Handles the submit of a new comment
@@ -10,17 +10,25 @@ const commentEventHandler = () => {
     const commentSection = document.querySelector('.detail-comments-section');
     const sendButton = document.querySelector('.send-comment-button');    
     const commentCounterIcon = document.querySelector('#comment-counter-container');
+
     
     sendButton.addEventListener('click', async evt => {
         evt.preventDefault();
         const commentText = document.querySelector('.create-comment-textarea');
         const beerId = document.querySelector('#beer-id').value;
 
-        if(commentText.value !== "") {
-            renderLoader('hide', 'show');
-            await setBeerComment( beerId, commentText.value );
-            window.location.reload();
+        const spanError = document.querySelector('.detail-comments-creation-section span.error');
+
+        if (commentText.value === "") { 
+            if(spanError.classList.contains('hide'))
+                toggle(spanError)('hide', 'show');
+            return;
         }
+        
+        toggle(spanError)('show', 'hide');
+        renderLoader('hide', 'show');
+        await setBeerComment( beerId, commentText.value );
+        window.location.reload();
     });
 
     commentCounterIcon.addEventListener('click', () => {
